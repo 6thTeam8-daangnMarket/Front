55fc038793a6c32ad75e9cc58be258d96a66673f
lee_chun <lcg5320@gmail.com>
2022-04-16 17:45:09 +0900

    로그인, 회원가입 기능 구현 완료

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,40 +1,48 @@
 import React from "react";
 import styled from "styled-components";
-import Button from '@mui/material/Button';
+import Button from "@mui/material/Button";
 import Text2 from "../elements/Text2";
 import { fontSize, fontWeight } from "@mui/system";
 
 const Header = (props) => {
-  const isLogin = sessionStorage.getItem('token') ? true: false;
-  if(!isLogin) {
+  const isLogin = localStorage.getItem("token") ? true : false;
+  if (!isLogin) {
     return (
       <React.Fragment>
         <HeaderWrap>
-          <img src="Danggeun_logo.png" style={{width: "300px"}}></img>
+          <img src="Danggeun_logo.png" style={{ width: "300px" }}></img>
           <ButtonWrap>
-            <Button style={{color: "#FF9F57", fontSize:"1em"}}>로그인</Button>
-            <Button style={{color: "#FF9F57", fontSize:"1em"}}>회원가입</Button>
+            <Button style={{ color: "#FF9F57", fontSize: "1em" }}>
+              로그인
+            </Button>
+            <Button style={{ color: "#FF9F57", fontSize: "1em" }}>
+              회원가입
+            </Button>
           </ButtonWrap>
         </HeaderWrap>
         <Bar></Bar>
       </React.Fragment>
-      
-    )
-  }
-  else {
+    );
+  } else {
     return (
       <React.Fragment>
         <HeaderWrap>
-          <img src="Danggeun_logo.png" style={{width: "300px"}}></img>
+          <img src="Danggeun_logo.png" style={{ width: "300px" }}></img>
           <ButtonWrap2>
-            <Text2 fontSize="1em" display="flex" alignItems="center" >name 님</Text2>
-            <Button style={{color: "#FF9F57", fontSize:"1em"}}>내 정보</Button>
-            <Button style={{color: "#FF9F57", fontSize:"1em"}}>로그아웃</Button>
+            <Text2 fontSize="1em" display="flex" alignItems="center">
+              name 님
+            </Text2>
+            <Button style={{ color: "#FF9F57", fontSize: "1em" }}>
+              내 정보
+            </Button>
+            <Button style={{ color: "#FF9F57", fontSize: "1em" }}>
+              로그아웃
+            </Button>
           </ButtonWrap2>
         </HeaderWrap>
         <Bar></Bar>
       </React.Fragment>
-    )
+    );
   }
 };
 const HeaderWrap = styled.div`
@@ -44,23 +52,23 @@ const HeaderWrap = styled.div`
   align-items: center;
   display: flex;
   justify-content: center;
-`
+`;
 const ButtonWrap = styled.div`
   position: absolute;
   right: 32px;
   display: grid;
   grid-template-columns: 1fr 1fr;
-`
+`;
 const ButtonWrap2 = styled.div`
   position: absolute;
   right: 32px;
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
-`
+`;
 const Bar = styled.div`
   margin: 0px auto;
   width: 95%;
   height: 1px;
   background-color: lightgrey; // #FF9F57;
-`
+`;
 export default Header;
--- src/elements/Grid.js
+++ src/elements/Grid.js
@@ -1,7 +1,66 @@
 import React from "react";
+import styled from "styled-components";
 
-const Grid = () => {
-  return <div></div>;
+const Grid = (props) => {
+  const {
+    children,
+    width,
+    height,
+    shadow,
+    bg,
+    margin,
+    padding,
+    is_flex,
+    center,
+    _onClick,
+  } = props;
+
+  const styles = {
+    width,
+    height,
+    shadow,
+    bg,
+    margin,
+    padding,
+    is_flex,
+    center,
+  };
+  return (
+    <React.Fragment>
+      <GridBox {...styles} onClick={_onClick}>
+        {children}
+      </GridBox>
+    </React.Fragment>
+  );
 };
+
+Grid.defaultProps = {
+  chidren: null,
+  width: "100vw",
+  height: "100vh",
+  shadow: "1px 4px 4px 4px rgba(0, 0, 0, 0.15)",
+  bg: false,
+  padding: false,
+  margin: false,
+  is_flex: false,
+  center: "center",
+  _onClick: () => {},
+};
+
+const GridBox = styled.div`
+  min-width: 300px;
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
+  box-sizing: border-box;
+  box-shadow: ${(props) => props.shadow};
+  ${(props) => (props.bg ? `background-color: ${props.bg};` : "")}
+  ${(props) => (props.padding ? `padding: ${props.padding};` : "")}
+    ${(props) => (props.margin ? `margin: ${props.margin};` : "")}
+    ${(props) =>
+    props.is_flex
+      ? `display: flex; align-items: center; justify-content: center;`
+      : ""};
+  ${(props) => (props.center ? `text-align: center;` : "")};
+`;
 
 export default Grid;
